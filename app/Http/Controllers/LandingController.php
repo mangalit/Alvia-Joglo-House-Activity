500c9442d59c3a6a59b9142eae72b8d2d1b34651
bagas2ndacc <bagas2ndacc@gmail.com>
2026-05-22 02:06:26 +0700

    route

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -8,7 +8,31 @@ class LandingController extends Controller
 {
     public function index()
     {
-        // Mengembalikan tampilan dari file landing_page.blade.php
         return view('landing_page');
+    }
+
+    public function activity()
+    {
+        return view('activity_page');
+    }
+
+    public function tracking()
+    {
+        return view('alvia_tracking_trip_page');
+    }
+
+    public function tour()
+    {
+        return view('tour_activity_page');
+    }
+
+    public function transport()
+    {
+        return view('transport_page');
+    }
+
+    public function about()
+    {
+        return view('about_&_contact_page');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 
-// Saat user mengakses URL utama '/', arahkan ke LandingController fungsi index
 Route::get('/', [LandingController::class, 'index'])->name('home');
+Route::get('/activity', [LandingController::class, 'activity'])->name('activity');
+Route::get('/alvia-tracking-trip', [LandingController::class, 'tracking'])->name('tracking');
+Route::get('/tour-activity', [LandingController::class, 'tour'])->name('tour');
+Route::get('/transport', [LandingController::class, 'transport'])->name('transport');
+Route::get('/about', [LandingController::class, 'about'])->name('about');
