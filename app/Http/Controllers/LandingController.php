238209284ae1b0e4cc90377c917b5e0c8a206181
mangalittt <komangalitt1010@gmail.com>
2026-05-27 01:14:16 +0700

    kode baru

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -10,29 +10,4 @@ class LandingController extends Controller
     {
         return view('landing_page');
     }
-
-    public function activity()
-    {
-        return view('activity_page');
-    }
-
-    public function tracking()
-    {
-        return view('alvia_tracking_trip_page');
-    }
-
-    public function tour()
-    {
-        return view('tour_activity_page');
-    }
-
-    public function transport()
-    {
-        return view('transport_page');
-    }
-
-    public function about()
-    {
-        return view('about_&_contact_page');
-    }
 }
