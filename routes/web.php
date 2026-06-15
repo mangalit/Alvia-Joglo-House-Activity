70606648fd7269ac90af47ac66b6ae431e000160
mangalittt <komangalitt1010@gmail.com>
2026-06-15 19:14:09 +0700

    deploy

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,22 @@ use App\Http\Controllers\ChatbotController;
 
 
 Route::get('/', [LandingController::class, 'home'])->name('home');
+
+// Deployment Helper (Hapus atau beri komentar setelah digunakan di Hostinger)
+// Akses: domainanda.com/deploy-helper?token=alvia-deploy-2026
+Route::get('/deploy-helper', function () {
+    if (request()->get('token') !== 'alvia-deploy-2026') {
+        return 'Unauthorized';
+    }
+    try {
+        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+        \Illuminate\Support\Facades\Artisan::call('storage:link');
+        return 'Migration and Storage Link successful!';
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
+});
+
 Route::get('/activity', [ActivityController::class, 'activity'])->name('activity');
 Route::get('/tracking', [TrackingController::class, 'tracking'])->name('tracking');
 Route::get('/tour', [TourController::class, 'tour'])->name('tour');
